30477e1bd722d736f61ace36c79a92ff7eaaf520
José Luis <46960705+joseluisald@users.noreply.github.com>
2023-03-28 11:26:09 -0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'joseluisald/instagram_api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'db10b2bc7ba9fcfd65c365e83735a57cac4cfb48',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'joseluisald/instagram_api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'db10b2bc7ba9fcfd65c365e83735a57cac4cfb48',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
